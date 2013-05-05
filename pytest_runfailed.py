dmerejkowsky/pytest-runfailed

commit c8af625aa4a28464779e87f2125bb52f7db7de40

python 3 fixes

pytest_runfailed.py
@@ -22,9 +22,10 @@ class RunFailed(object):
     def __init__(self, config):
         self.config = config
         cwd = py.path.local(os.getcwd())
-        self.pickle_path = cwd.ensure(".pytest", "failed")
+        self.pickle_path = cwd.ensure(".pytest", "failed").strpath
         try:
-            self.failed = pickle.loads(self.pickle_path.read())
+            with open(self.pickle_path, "rb") as fp:
+                self.failed = pickle.load(fp)
         except EOFError:
             self.failed = set()
 
@@ -36,8 +37,9 @@ class RunFailed(object):
     def pytest_runtestloop(self, session):
         if not self.failed:
             return
-        print "running %s previously failing test(s)" % len(session.items)
+        print("running %s previously failing test(s)" % len(session.items))
 
     def pytest_terminal_summary(self, terminalreporter):
-        failed = terminalreporter.stats.get("failed", list())
-        self.pickle_path.dump([x.nodeid for x in failed])
+        self.failed = terminalreporter.stats.get("failed", list())
+        with open(self.pickle_path, "wb") as fp:
+            pickle.dump(self.failed, fp)

tests/test_run_failed.py
@@ -13,18 +13,12 @@ def test_fail():
 
 
 def test_stores_failures(testdir):
-    testdir.makeconftest("""
-pytest_plugins = "pytest_runfailed"
-""")
     testdir.makepyfile(test_pass=passing_test, test_fail=failing_test)
     reprec = testdir.inline_run("--failed")
     passed, skipped, failed = reprec.listoutcomes()
     assert testdir.tmpdir.join(".pytest", "failed").check(file=1)
 
 def test_run_only_failures(testdir):
-    testdir.makeconftest("""
-pytest_plugins = "pytest_runfailed"
-""")
     testdir.makepyfile(test_pass=passing_test, test_fail=failing_test)
     testdir.inline_run("--failed")
     reprec = testdir.inline_run("--failed")

setup.py
@@ -4,7 +4,7 @@ setup(
     name='pytest-runfailed',
     license='BSD',
     description='implement a --failed option for pytest',
-    version='0.2',
+    version='0.3',
     author='Dimitri Merejkowsky',
     author_email='d.merej@gmail.com',
     url='http://github.com/dmerejkowsky/pytest-runfailed',

commit 19b4c930995d6f4fc860a63e45c82972b920b04a

Fix wrong data format being dumped
c8af625aa4 introduced a regression, no failing tests were found.
Tests weren't catching the problem, so I also fixed the tests.

pytest_runfailed.py
@@ -42,4 +42,4 @@ class RunFailed(object):
     def pytest_terminal_summary(self, terminalreporter):
         self.failed = terminalreporter.stats.get("failed", list())
         with open(self.pickle_path, "wb") as fp:
-            pickle.dump(self.failed, fp)
+            pickle.dump([x.nodeid for x in self.failed], fp)

tests/test_run_failed.py
@@ -24,3 +24,5 @@ def test_run_only_failures(testdir):
     reprec = testdir.inline_run("--failed")
     passed, skipped, failed = reprec.listoutcomes()
     assert len(passed) == 0
+    assert len(skipped) == 0
+    assert len(failed) == 1